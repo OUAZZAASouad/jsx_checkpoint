54234472aec0ab0f51ab6f3e1aef1359331ca021
OUAZZAA Souad <souad.ouazzaa@it.devcorp.fr>
2021-04-10 22:51:35 +0100

    style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
 import React from "react";
 import imageInSrc from "./images/imageInSrc.jpg"
-import mystyle from "./style.css"
+import "./style.css"
 
 const App = () => {
   return (
     <React.Fragment>
-      <div style = {{border: "solid 1px gray" , maxWidth:"100vw", boxShadow : '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
-        <h1 className ="title red" style = {{mystyle}}>Your name here</h1>
+      <div className = 'container' >
+        <h1 className ="title red" >Your name here</h1>
         <div className = 'row'>
-          <img src = {imageInSrc}  width="320" height="200" style = {{padding : 10}} />
+          <img src = {imageInSrc}  width="320" height="200"/>
         </div>
 
         <div className = 'row'>
-          <img src="/imageInPublic.jpg"  width="320"  height="200" style = {{padding : 10}} />
+          <img src="/imageInPublic.jpg"  width="320"  height="200"/>
         </div>
         
       </div>
 
-      <video width="320" height="240" controls style = {{padding : 10}}>
+      <video width="320" height="240" controls>
         <source src="myVideo.mp4" type="video/mp4" />
       </video>
    </React.Fragment>
