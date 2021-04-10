f386eedd323c136b4414d753da4f4e42470218f7
OUAZZAA Souad <souad.ouazzaa@it.devcorp.fr>
2021-04-10 22:25:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import imageInSrc from "./images/imageInSrc.jpg"
+import mystyle from "./style.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div style = {{border: "solid 1px gray" , maxWidth:"100vw", boxShadow : '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
+        <h1 className ="title red" style = {{mystyle}}>Your name here</h1>
+        <div className = 'row'>
+          <img src = {imageInSrc}  width="320" height="200" style = {{padding : 10}} />
+        </div>
+
+        <div className = 'row'>
+          <img src="/imageInPublic.jpg"  width="320"  height="200" style = {{padding : 10}} />
+        </div>
+        
+      </div>
+
+      <video width="320" height="240" controls style = {{padding : 10}}>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
+   </React.Fragment>
   );
 }
 
